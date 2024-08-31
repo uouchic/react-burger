d12f6dc4e42ee071bf9f74affba60625664ec59c
Vladimir Chehnousov <tehnokiparis@mail.ru>
2024-08-31 18:44:42 +0300

    ПР1 добавлен запрос к АПИ

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './app.module.css';
 
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 
-import { INGREDIENTS } from '../../utils/data';
+const BASE_URL = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
-  const [ingridients, setIngridients] = useState(INGREDIENTS);
+  const [ingridients, setIngridients] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setError(null);
+    const getIngredientsData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(BASE_URL);
+        const ingredients = await res.json();
+        setIngridients(ingredients.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err);
+        setLoading(false);
+      }
+    };
+    getIngredientsData();
+  }, []);
 
   return (
     <div className={styles.app}>
       <AppHeader />
-      <main className={`${styles.content} pb-10`}>
-        <BurgerIngredients ingridients={ingridients} />
-        <BurgerConstructor />
-      </main>
+
+      {loading ? (
+        <h2 className={`${styles.mess} mt-10`}>
+          Загрузка ингридиентов бургера
+        </h2>
+      ) : error ? (
+        <h2 className={`${styles.mess} mt-10`}>
+          Что-то пошло не так:<p>{error.message}</p>
+        </h2>
+      ) : (
+        <main className={`${styles.content} pb-10`}>
+          <BurgerIngredients ingridients={ingridients} />
+          <BurgerConstructor />
+        </main>
+      )}
     </div>
   );
 }
